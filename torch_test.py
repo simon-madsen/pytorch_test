b81091ed30f9a8c963971b0dbe2bc2d77d206ecf
simon-madsen <143513065+simon-madsen@users.noreply.github.com>
2023-08-29 14:43:23 +0200

    Update torch_test.py

--- torch_test.py
+++ torch_test.py
@@ -1,1 +1,27 @@
+import pytorch
+from torch import nn
 
+# Ensures correct device is selected
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+class model_blueprint:
+    def __init__(self, input_features, output_features, hidden_units=8):
+        """Initializes all required hyperparameters for a multi-class classification model.
+
+        Args:
+            input_features (int): Number of input features to the model.
+            out_features (int): Number of output features of the model
+              (how many classes there are).
+            hidden_units (int): Number of hidden units between layers, default 8.
+        """
+        super().__init__()
+        self.linear_layer_stack = nn.Sequential(
+            nn.Linear(in_features=input_features, out_features=hidden_units),
+            nn.ReLU(),
+            nn.Linear(in_features=hidden_units, out_features=hidden_units),
+            nn.ReLU(), 
+            nn.Linear(in_features=hidden_units, out_features=output_features),  # how many classes are there?
+        )
+
+    def forward(self, x):
+        return self.linear_layer_stack(x)
